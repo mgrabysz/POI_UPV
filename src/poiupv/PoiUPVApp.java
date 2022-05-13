978ef456389e86f719e7ab7ed3ecbef966efefb8
mgrabysz <margrabysz@gmail.com>
2022-05-13 16:33:12 +0200

    Set minimal width and height

--- src/poiupv/PoiUPVApp.java
+++ src/poiupv/PoiUPVApp.java
@@ -24,6 +24,8 @@ public class PoiUPVApp extends Application {
         Scene scene = new Scene(root);
         stage.setTitle("Carta nautica");
         stage.setScene(scene);
+        stage.setMinWidth(800);
+        stage.setMinHeight(600);
         stage.show();
     }
 
